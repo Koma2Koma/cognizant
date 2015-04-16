2487b67ae641411aae191450a246868e84e40024
Konrad Maurer <kradjr@gmail.com>
2015-04-15 20:19:09 -0600

    Adds about page and begins organizing nav-bar

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   
   get 'home' => 'static_pages#home', as: :home
 
+  get 'about' => 'static_pages#about', as: :about
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
