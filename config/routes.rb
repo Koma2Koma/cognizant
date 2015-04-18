52b87450cf54fdd3c7af1f6d5256a8ab46cd528c
Konrad Maurer <kradjr@gmail.com>
2015-04-18 12:17:05 -0600

    devise with user model installed.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
 
+  root to: 'static_pages#home'
 
+  devise_for :users
+  
   get 'home' => 'static_pages#home', as: :home
 
   get 'about' => 'static_pages#about', as: :about
