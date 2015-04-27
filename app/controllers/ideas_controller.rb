7f87db76dfe486976ed19e1a596c230d0f474293
Konrad Maurer <kradjr@gmail.com>
2015-04-27 10:15:47 -0600

    Add new idea functionality, modifies idea show and topic show to view ideas.

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -1,9 +1,12 @@
 class IdeasController < ApplicationController
-  before_action :set_category
+  #before_action :set_category, except: [:new, :create]
+  before_action :set_topic, except: :show
   before_action :set_idea, except: [:index, :new, :create]
 
 
   def show
+    @topic = Topic.find(@idea.topic_id)
+    @category = Category.find(@topic.category_id)
   end
 
   def index
@@ -11,16 +14,17 @@ class IdeasController < ApplicationController
 
   def new
     @idea = Idea.new
+    @category = Category.find(@topic.category_id)
   end
 
   def create
     @idea = Idea.create(idea_params)
-    @idea[:category_id] = @category.id
+    @idea[:topic_id] = @topic.id
 
     respond_to do |format|
       if @idea.save
         format.html { redirect_to [@category, @idea], notice: 'Idea successfully created.' }
-        format.json { render :show, status: :created, location: @idea}
+        format.json { render :show, status: :created, location: @idea }
       else
         format.html { render :new }
         format.json { render json: @idea.errors, status: :unprocessable_entity }
@@ -42,8 +46,13 @@ class IdeasController < ApplicationController
 
   private
 
+    ######## Maybe delete
     def set_category
       @category = Category.find(params[:category_id])
+    end
+
+    def set_topic
+      @topic = Topic.find(params[:topic_id])
     end
 
     def set_idea
